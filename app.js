3702b4ad1c434a7abcd374644268929e902b3770
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2019-05-22 10:12:10 +0200

    heroku website first commit

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express');
-
+const path = require('path');
+const pagesRoutes = require('./src/routes/pagesRoutes');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// set views folder
+app.set('views', path.join(__dirname, '/src/views'));
+// set view engine
+app.set('view engine', 'ejs');
+
+// set the public folder
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use('/', pagesRoutes);
 app.get('/', (req, res) => {
 res.send('I am Ahmad and Hello to my heroku website');
 });
---
+++ src/routes/pagesRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const pagesRoutes = express.Router();
+
+pagesRoutes.route('/').get((req, res)=>{
+res.render('index');
+});
+
+
+module.exports = pagesRoutes;
